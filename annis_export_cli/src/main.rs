e40e6d578e9f03faad5cec2f828288062f7eebb1
Matthias Stemmler <matthias.stemmler@gmail.com>
2023-11-26 16:49:20 +0100

    List all corpora

--- annis_export_cli/src/main.rs
+++ annis_export_cli/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    annis_export_core::main();
+use annis_export_core::CorpusStorage;
+use anyhow::Context;
+use clap::{Parser, Subcommand};
+use std::env;
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// List all corpora
+    List,
+}
+
+fn main() -> anyhow::Result<()> {
+    let cli = Cli::parse();
+
+    let db_dir =
+        env::var("ANNIS_DB_DIR").context("Environment variable `ANNIS_DB_DIR` is not set")?;
+
+    let corpus_storage = CorpusStorage::from_db_dir(&db_dir)
+        .with_context(|| format!("Failed to open corpus storage from {db_dir}"))?;
+
+    match cli.command {
+        Commands::List => {
+            for name in corpus_storage
+                .corpus_names()
+                .context("Failed to list corpora")?
+            {
+                println!("{name}");
+            }
+        }
+    }
+
+    Ok(())
 }
--- annis_export_core/src/lib.rs
+++ annis_export_core/src/lib.rs
@@ -1,3 +1,35 @@
-pub fn main() {
-    println!("Hello, world!");
+use std::{path::Path, vec};
+
+use graphannis::{
+    corpusstorage::{CacheStrategy, CorpusInfo},
+    errors::GraphAnnisError,
+};
+
+pub struct CorpusStorage(graphannis::CorpusStorage);
+
+impl CorpusStorage {
+    pub fn from_db_dir<P>(db_dir: P) -> Result<Self, GraphAnnisError>
+    where
+        P: AsRef<Path>,
+    {
+        Ok(Self(graphannis::CorpusStorage::with_cache_strategy(
+            db_dir.as_ref(),
+            CacheStrategy::PercentOfFreeMemory(25.0),
+            true,
+        )?))
+    }
+
+    pub fn corpus_names(&self) -> Result<CorpusNames, GraphAnnisError> {
+        Ok(CorpusNames(self.0.list()?.into_iter()))
+    }
+}
+
+pub struct CorpusNames(vec::IntoIter<CorpusInfo>);
+
+impl Iterator for CorpusNames {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(self.0.next()?.name)
+    }
 }
