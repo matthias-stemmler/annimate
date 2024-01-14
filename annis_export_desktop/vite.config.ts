a51127b7c5b182fd7c0935b6c573e6afa4329945
Matthias Stemmler <matthias.stemmler@gmail.com>
2024-01-14 14:30:25 +0100

    Add formatting and linting

--- annis_export_desktop/vite.config.ts
+++ annis_export_desktop/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
@@ -9,7 +9,7 @@ export default defineConfig(async () => ({
     port: 1420,
     strictPort: true,
     watch: {
-      ignored: ["**/src-tauri/**"],
+      ignored: ['**/src-tauri/**'],
     },
   },
 }));
