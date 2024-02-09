a53d45a637d06c4e08f002fd8a1e8d9da321420c
Matthias Stemmler <matthias.stemmler@gmail.com>
2024-02-09 23:16:13 +0100

    Add @tanstack/query ESLint plugin

--- annis_export_desktop/.eslintrc.cjs
+++ annis_export_desktop/.eslintrc.cjs
@@ -3,12 +3,13 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['@tanstack/query', 'react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
