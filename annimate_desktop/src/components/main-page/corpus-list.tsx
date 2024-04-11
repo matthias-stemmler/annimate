a8d99b03c8d5dfe997ef165808d74231da1ca9e0
Matthias Stemmler <matthias.stemmler@gmail.com>
2024-04-11 21:45:31 +0200

    Fix scrolling in corpus list

--- annimate_desktop/src/components/main-page/corpus-list.tsx
+++ annimate_desktop/src/components/main-page/corpus-list.tsx
@@ -26,7 +26,7 @@ export const CorpusList: FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-2 h-full">
       <div className="flex items-center gap-2">
         <CorpusSetSelect />
 
