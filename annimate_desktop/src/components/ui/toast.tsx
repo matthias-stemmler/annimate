abc3a0bcbcefda9fd00d9f2f6b2a90f17049f41b
Matthias Stemmler <matthias.stemmler@gmail.com>
2025-05-25 16:29:20 +0200

    Migrate Tailwind classes

--- annimate_desktop/src/components/ui/toast.tsx
+++ annimate_desktop/src/components/ui/toast.tsx
@@ -20,7 +20,7 @@ const ToastViewport: FC<ComponentProps<typeof ToastPrimitives.Viewport>> = ({
 );
 
 const toastVariants = cva(
-  'group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg focus:outline-hidden focus:ring focus:ring-ring focus:ring-offset-1 transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full sm:data-[state=open]:slide-in-from-bottom-full mt-1',
+  'group pointer-events-auto relative flex w-full items-center justify-between space-x-4 overflow-hidden rounded-md border p-6 pr-8 shadow-lg focus:outline-hidden focus:ring focus:ring-ring focus:ring-offset-1 transition-all data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-(--radix-toast-swipe-end-x) data-[swipe=move]:translate-x-(--radix-toast-swipe-move-x) data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full sm:data-[state=open]:slide-in-from-bottom-full mt-1',
   {
     variants: {
       variant: {
