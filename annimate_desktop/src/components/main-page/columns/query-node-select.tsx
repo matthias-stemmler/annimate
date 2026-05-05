084b61ba85786365a829f4e08453a1a0c6aed712
Matthias Stemmler <matthias.stemmler@gmail.com>
2026-05-05 20:18:54 +0200

    Extract QueryNodeSelect to separate module

---
+++ annimate_desktop/src/components/main-page/columns/query-node-select.tsx
@@ -0,0 +1,55 @@
+import { Select, SelectOption } from '@/components/ui/custom/select';
+import { QueryNodeRef } from '@/lib/api-types';
+import { useIsExporting, useQueryNodes } from '@/lib/store';
+import { FC } from 'react';
+import { QueryNodesDisplay } from '@/components/main-page/columns/query-nodes-display';
+
+export type QueryNodeSelectProps = {
+  id?: string;
+  nodeRef: QueryNodeRef | undefined;
+  onChange?: (nodeRef: QueryNodeRef) => void;
+};
+
+export const QueryNodeSelect: FC<QueryNodeSelectProps> = ({
+  id,
+  nodeRef,
+  onChange,
+}) => {
+  const { data: queryNodes, error, isPending } = useQueryNodes();
+  const isExporting = useIsExporting();
+  const disabled = isExporting;
+
+  if (error !== null) {
+    throw new Error(`Failed to determine query nodes: ${error.message}`);
+  }
+
+  const nodes = queryNodes?.type === 'valid' ? queryNodes.nodes : [];
+
+  return (
+    <Select
+      className="h-8"
+      disabled={disabled}
+      id={id}
+      loading={isPending}
+      onChange={(value) => {
+        const index = parseInt(value);
+        const group = nodes[index];
+        if (group === undefined) {
+          return;
+        }
+
+        onChange?.({
+          index,
+          variables: group.map((n) => n.variable),
+        });
+      }}
+      options={nodes.map(
+        (group, i): SelectOption<`${number}`> => ({
+          caption: <QueryNodesDisplay queryNodes={group} />,
+          value: `${i}`,
+        }),
+      )}
+      value={nodeRef === undefined ? undefined : `${nodeRef.index}`}
+    />
+  );
+};
